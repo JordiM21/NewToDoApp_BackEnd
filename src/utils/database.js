bcdd7aea543f95f1e8601925f472352bcb0dea54
Jordi Mantilla <jordimantilla21@gmail.com>
2022-11-10 11:48:47 +0100

    conecting with railway

--- src/utils/database.js
+++ src/utils/database.js
@@ -5,12 +5,12 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize({
-  database: process.env.DB_NAME || "todo",
-  username: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD || "root",
-  dialect: "postgres",
+	database: process.env.DB_NAME || "todo",
+	username: process.env.DB_USER || "postgres",
+	host: process.env.DB_HOST || "localhost",
+	port: process.env.DB_PORT || 5432,
+	password: process.env.DB_PASSWORD || "l0c0fl1n",
+	dialect: "postgres",
 });
 
 module.exports = db;
